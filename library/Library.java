e984756d767fcbccf650cd8e4fcb32c834616d50
Azzahra Dinda <azzahradindas@gmail.com>
2020-03-03 08:00:21 +0700

    add a review for easier understanding

--- library/Library.java
+++ library/Library.java
@@ -4,10 +4,13 @@ class Library {
   public ArrayList<Book> books = new ArrayList<Book>();
   public ArrayList<Member> members = new ArrayList<Member>();
 
+  //menambah member kedalam array list
   public void addMember(Member member) {
     this.members.add(member);
   }
 
+
+  //looping mencari id member
   public Boolean isMemberIdExist(String id) {
     Boolean isExist = false;
     for (Member member : this.members) {
@@ -18,24 +21,30 @@ class Library {
     return isExist;
   }
 
+  //meminjamkan buku apabila id buku dan id member ditemukan
   public void giveBook(String bookId, String memberId) {
+    //mencari buku lalu meremove buku dari arraylist buku
     Book book = this.getBookById(bookId);
     this.books.remove(book);
 
+    //mencari  nomer member dan index lalu 
     Member member = this.getMemberById(memberId);
     int memberIndex = this.getMemberIndex(member);
     this.members.get(memberIndex).borrowedBooks.add(book);
   }
 
   public void receiveBook(String bookId, String memberId) {
+    //menambahkan book kedalam array list books
     Book book = this.getBookById(bookId);
     this.books.add(book);
 
+    //mencari nomer member dan index member, lalu menghilangkan dari member
     Member member = this.getMemberById(memberId);
     int memberIndex = this.getMemberIndex(member);
     this.members.get(memberIndex).borrowedBooks.remove(book);
   }
 
+  
   private int getMemberIndex(Member member) {
     return this.members.indexOf(member);
   }
@@ -50,7 +59,9 @@ class Library {
   }
 
   private Book getBookById(String id) {
+    //for every book from arraylist books.
     for (Book book : this.books) {
+      //if book.id equals id we need to return the book.
       if (book.id.equals(id)) {
         return book;
       }
--- library/Member.java
+++ library/Member.java
@@ -4,11 +4,13 @@ class Member {
   public String id;
   public String name;
   public ArrayList<Book> borrowedBooks = new ArrayList<Book>();
-
+  
+  //menerima book kedalam array list
   public void receiveBook(Book book) {
     this.borrowedBooks.add(book);
   }
 
+  //mengeluarkan book dari member
   public void giveBook(Book book) {
     this.borrowedBooks.remove(book);
   }
